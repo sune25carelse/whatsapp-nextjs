b31af081607bb6512afbd609333bffa5ab7c7f8f
Sune Carelse <sune.carelse@takealot.com>
2021-04-05 20:44:38 +0200

    serach icon/input and

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
-import { Avatar, Icon, IconButton } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Sidebar() {
   return (
@@ -17,6 +18,10 @@ function Sidebar() {
           </IconButton>
         </IconsContainer>
       </Header>
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
     </Container>
   );
 }
@@ -25,6 +30,19 @@ export default Sidebar;
 
 const Container = styled.div``;
 
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-radius: 2px;
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
 const Header = styled.div`
   display: flex;
   position: sticky;
@@ -32,10 +50,18 @@ const Header = styled.div`
   background-color: white;
   z-index: 1;
   justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
 `;
 
 const UserAvatar = styled(Avatar)`
-  margin: 10px;
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
 `;
 
 const IconsContainer = styled.div``;
