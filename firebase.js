bc62252d76b9e7893657975a5b06c7b56e423076
Sune Carelse <sune.carelse@takealot.com>
2021-04-05 21:34:31 +0200

    firebase config, initializ app with db, auth prvdr

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+// config goes here
+
+// connection to database setup goes here initialize first
+const app = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const db = app.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
