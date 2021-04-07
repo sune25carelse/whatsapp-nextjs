a66085bf4fd3358651ff79dde2eb2992d6ed594d
Sune Carelse <sune.carelse@takealot.com>
2021-04-07 21:57:06 +0200

    ggle auth signin popup and alert function

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,13 @@
 import { Button } from "@material-ui/core";
 import Head from "next/head";
 import styled from "styled-components";
+import { auth, provider } from "../firebase";
 
 function Login() {
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch(alert);
+  };
+
   return (
     <Container>
       <Head>
@@ -11,7 +16,9 @@ function Login() {
 
       <LoginContainer>
         <Logo src="https://seeklogo.net/wp-content/uploads/2012/11/liverbir-vector-logo-400x400.png" />
-        <Button variant="outlined">Sign in with Google</Button>
+        <Button onClick={signIn} variant="outlined">
+          Sign in with Google
+        </Button>
       </LoginContainer>
     </Container>
   );
@@ -23,11 +30,17 @@ const Container = styled.div`
   display: grid;
   place-items: center;
   height: 100vh;
+  background-color: whitesmoke;
 `;
 
 const LoginContainer = styled.div`
+  padding: 100px;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.7);
 `;
 
 const Logo = styled.img`
