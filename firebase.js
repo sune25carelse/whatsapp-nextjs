183dd2eb7a3d7beefc69467185c451bf200b66a3
Sune Carelse <sune.carelse@takealot.com>
2021-04-07 21:04:23 +0200

    firebase config updated

--- firebase.js
+++ firebase.js
@@ -1,7 +1,14 @@
 import firebase from "firebase";
+
 // config goes here
-
-
+const firebaseConfig = {
+  apiKey: "AIzaSyASs7T01a5ma4I_vgXQOaYMwIx_c3vV6Ns",
+  authDomain: "whatsapp-2-f032c.firebaseapp.com",
+  projectId: "whatsapp-2-f032c",
+  storageBucket: "whatsapp-2-f032c.appspot.com",
+  messagingSenderId: "742548566841",
+  appId: "1:742548566841:web:27ade6d80228a63b102ee8",
+};
 
 // connection to database setup goes here initialize first
 const app = !firebase.apps.length
