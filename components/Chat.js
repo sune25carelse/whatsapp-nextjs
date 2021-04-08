4a4778a11dbdb7ea41873c18f7792675fda43794
Sune Carelse <sune.carelse@takealot.com>
2021-04-08 21:06:48 +0200

    chat avatars and style

--- components/Chat.js
+++ components/Chat.js
@@ -1,8 +1,10 @@
+import { Avatar } from "@material-ui/core";
 import styled from "styled-components";
 
 function Chat({ id, users }) {
   return (
     <Container>
+      <UserAvatar />
       <p>Recipients Email</p>
     </Container>
   );
@@ -10,4 +12,19 @@ function Chat({ id, users }) {
 
 export default Chat;
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 15px;
+  word-break: break-word;
+
+  :hover {
+    background-color: #e9eaeb;
+  }
+`;
+
+const UserAvatar = styled(Avatar)`
+  margin: 5px;
+  margin-right: 15px;
+`;
