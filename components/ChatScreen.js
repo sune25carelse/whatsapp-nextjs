eee80234f8b0a9ceef00db602660d81f9a0c4a71
Sune Carelse <sune.carelse@takealot.com>
2021-04-11 19:50:01 +0200

    chatscreen with state for user & avatar etc creatd

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,9 +1,22 @@
+import { Avatar } from "@material-ui/core";
+import { useRouter } from "next/router";
+import { useAuthState } from "react-firebase-hooks/auth";
 import styled from "styled-components";
+import { auth } from "../firebase";
 
-function ChatScreen() {
+function ChatScreen({ chat, messages }) {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+
   return (
     <Container>
-      <h1>This is a chat </h1>
+      <Header>
+        <Avatar />
+
+        <HeaderInformation>
+          <h3>Recipient</h3>
+        </HeaderInformation>
+      </Header>
     </Container>
   );
 }
@@ -11,3 +24,7 @@ function ChatScreen() {
 export default ChatScreen;
 
 const Container = styled.div``;
+
+const Header = styled.div``;
+
+const HeaderInformation = styled.div``;
